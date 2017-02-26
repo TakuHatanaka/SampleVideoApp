0b0aff6ec2e52a9b014f9c2738aec18cbb0df949
Taku Hatanaka <taku-hatanaka@be-org.co.jp>
2017-02-26 16:55:47 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleVideoApp/SampleVideoApp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SampleVideoApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

342de26972463dc26cdd9fc2727702387ef848ac
Taku Hatanaka <taku-hatanaka@be-org.co.jp>
2017-02-26 17:15:42 +0900

    updates

--- SampleVideoApp/SampleVideoApp/ViewModels/MainPageViewModel.cs
+++ SampleVideoApp/SampleVideoApp/ViewModels/MainPageViewModel.cs
@@ -1,35 +1,38 @@
-﻿using Prism.Commands;
+﻿using System.Windows.Input;
 using Prism.Mvvm;
-using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Xamarin.Forms;
 
 namespace SampleVideoApp.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
+    /// <summary>
+    /// 動画再生サンプルページのViewModelクラス
+    /// </summary>
+    public class MainPageViewModel : BindableBase
     {
-        private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainPageViewModel()
         {
 
         }
 
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
+        /// <summary>
+        /// 再生ボタンクリックイベントコマンド
+        /// </summary>
+        private ICommand _BtnPlayClickedCommand = null;
+        /// <summary>
+        /// 再生ボタンクリックイベントコマンド
+        /// </summary>
+        public ICommand BtnPlayClickedCommand => _BtnPlayClickedCommand ?? (
+            _BtnPlayClickedCommand = new Command(
+                () => ExecuteBtnPlayClicked()));
 
-        }
-
-        public void OnNavigatedTo(NavigationParameters parameters)
+        /// <summary>
+        /// 再生ボタンクリック処理
+        /// </summary>
+        private void ExecuteBtnPlayClicked()
         {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
         }
     }
 }
